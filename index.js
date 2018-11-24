41144472d0d94070ffe03396725b91a9ba4fd88b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-24 07:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj,key,value){
+<<<<<<< HEAD
   return Object.assign({}, obj, { [key]: value });
+=======
+  return Object.assign(obj, {[key]:value});
+>>>>>>> b6fa9f18c351c6ae98c2c700862bb8341f0d1261
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
@@ -10,7 +14,11 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromObjectByKey(obj,key){
+<<<<<<< HEAD
   var newObj = Object.assign({},obj);
+=======
+  var newObj = obj;
+>>>>>>> b6fa9f18c351c6ae98c2c700862bb8341f0d1261
   delete newObj[key];
   return newObj;
 }
